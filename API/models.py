1900347930af992f353c04e733edabe89656c5bc
YaserAlqasem <alqasemyaser@gmial.com>
2020-02-28 08:43:52 -0800

    Add User Model

--- API/models.py
+++ API/models.py
@@ -1,6 +1,27 @@
 from marshmallow import Schema, fields, pre_load, validate
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-
+import datetime
 ma = Marshmallow()
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    Username = db.Column(db.String(150), unique=True, nullable=False)
+    Email = db.Column(db.String(150), nullable=False)
+    Password = db.Column(db.String(255), nullable=False)
+    Registered_on = db.Column(db.DateTime, nullable=False)
+    def __init__(self, Username, Email,Password):
+        self.Username = Username
+        self.Email = Email
+        self.Password = Password
+        self.Registered_on = datetime.datetime.now()
+        
+class UserSchema(ma.Schema):
+    id = fields.Integer()
+    Username = fields.String(required=True)
+    Email = fields.String(required=False)
+    Password = fields.String(required=True)
+    Registered_on = fields.DateTime()
