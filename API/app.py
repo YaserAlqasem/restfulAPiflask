e0ce6779c627e2eb17af52dc839c68f1e8776ddf
YaserAlqasem <alqasemyaser@gmial.com>
2020-02-28 10:06:37 -0800

    Add UsersList Resource Route to app.py

--- API/app.py
+++ API/app.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources.User import SignUp,Login,UserLogoutAccess,UserLogoutRefresh,TokenRefresh
+from resources.User import SignUp,Login,UserLogoutAccess,UserLogoutRefresh,TokenRefresh,UsersList
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -11,3 +11,4 @@ api.add_resource(Login, '/auth')
 api.add_resource(UserLogoutAccess,'/logout1')
 api.add_resource(UserLogoutRefresh,'/logout2')
 api.add_resource(TokenRefresh,'/refreshToken')
+api.add_resource(UsersList, '/users')
