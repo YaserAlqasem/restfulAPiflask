d68ecbd5d58637193fd3c20e2b8c7e183c9ffcf7
YaserAlqasem <alqasemyaser@gmial.com>
2020-02-28 10:09:47 -0800

    Add UserById  Resource Route to app.py

--- API/app.py
+++ API/app.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources.User import SignUp,Login,UserLogoutAccess,UserLogoutRefresh,TokenRefresh,UsersList
+from resources.User import SignUp,Login,UserLogoutAccess,UserLogoutRefresh,TokenRefresh,UsersList,UserById
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -12,3 +12,4 @@ api.add_resource(UserLogoutAccess,'/logout1')
 api.add_resource(UserLogoutRefresh,'/logout2')
 api.add_resource(TokenRefresh,'/refreshToken')
 api.add_resource(UsersList, '/users')
+api.add_resource(UserById, '/user/<userid>')
