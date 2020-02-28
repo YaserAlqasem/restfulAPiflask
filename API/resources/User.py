00235495021f1a5308968295e09a016bf500a964
YaserAlqasem <alqasemyaser@gmial.com>
2020-02-28 09:21:41 -0800

    Add Login Resource to User.py

--- API/resources/User.py
+++ API/resources/User.py
@@ -2,7 +2,8 @@ from flask_restful import Resource
 from flask import jsonify,json
 from flask import flash, request
 from models import db, User, UserSchema
-
+import bcrypt
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 
 users_schema = UserSchema(many=True)
 user_schema = UserSchema()
@@ -36,3 +37,31 @@ class SignUp(Resource):
                     }
             except:
                 return {'message': 'Something went wrong'}, 500
+
+class Login(Resource):
+    def post(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+               return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data, errors = user_schema.load(json_data)
+        if errors:
+            return errors, 422
+
+        print(json_data['Password'].encode('utf8'))
+        print(json_data['Password'])
+        found_user = User.query.filter_by(Username = json_data['Username']).first()
+        
+        if found_user:
+            authenticated_user = bcrypt.checkpw(json_data['Password'].encode('utf8'),found_user.Password.encode("utf-8"))
+            
+            access_token = create_access_token(identity = json_data['Username'])
+            refresh_token = create_refresh_token(identity = json_data['Username'])
+            if authenticated_user:
+                return {
+                    'message': 'Logged in as {}'.format(found_user.Username),
+                    'access_token': access_token,
+                    'refresh_token': refresh_token
+                    },200
+            else:
+                return {'message': 'Wrong credentials'}, 401
