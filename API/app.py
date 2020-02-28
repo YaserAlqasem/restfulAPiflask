a00afa44f308098dac78d81803f766efbc523167
YaserAlqasem <alqasemyaser@gmial.com>
2020-02-28 10:02:43 -0800

    Add TokenRefresh Resource Route to app.py

--- API/app.py
+++ API/app.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources.User import SignUp,Login,UserLogoutAccess,UserLogoutRefresh
+from resources.User import SignUp,Login,UserLogoutAccess,UserLogoutRefresh,TokenRefresh
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -10,3 +10,4 @@ api.add_resource(SignUp, '/register')
 api.add_resource(Login, '/auth')
 api.add_resource(UserLogoutAccess,'/logout1')
 api.add_resource(UserLogoutRefresh,'/logout2')
+api.add_resource(TokenRefresh,'/refreshToken')
