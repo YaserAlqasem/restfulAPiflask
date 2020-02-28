75ee50df4541613c3aca0a0941b5ee1740bd7df7
YaserAlqasem <alqasemyaser@gmial.com>
2020-02-28 09:22:34 -0800

    Add Login Resource Route to app.py

--- API/app.py
+++ API/app.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources.User import SignUp
+from resources.User import SignUp,Login
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # Route
 api.add_resource(SignUp, '/register')
+api.add_resource(Login, '/auth')
